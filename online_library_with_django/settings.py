68f93604633747a8cd027a799edea5ce96bf0611
jurakhon <jurakhon@gmail.com>
2024-12-27 04:01:23 +0500

    css

--- online_library_with_django/settings.py
+++ online_library_with_django/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#    BASE_DIR / "static",
-# ]
+STATICFILES_DIRS = [
+   BASE_DIR / "staticfiles",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
